b5cda149e6212ae32b41fa88e51ccd3d6aa79251
keep2iron <497881309@qq.com>
2018-10-24 21:04:53 +0800

    1.重构代码组织结构

---
+++ lib-core/src/main/java/io/github/keep2iron/android/core/AbstractSwipeBackActivity.kt
@@ -0,0 +1,9 @@
+package io.github.keep2iron.android.core
+
+import android.databinding.ViewDataBinding
+
+/**
+ * @author keep2iron
+ */
+abstract class AbstractSwipeBackActivity<DB : ViewDataBinding> : AbstractActivity<DB>() {
+}
--- lib-network/src/main/java/io/github/keep2iron/network/IResponseStatus.kt
+++
@@ -1,8 +0,0 @@
-package io.github.keep2iron.network
-
-interface IResponseStatus {
-    /**
-     * @return 是否响应成功
-     */
-    val isResponseSuccessful: Boolean
-}
